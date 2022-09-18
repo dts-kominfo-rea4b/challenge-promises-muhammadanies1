26df516b0bf2abff079e0b4ead555ef919d9ed70
Muhammad Anies Wahdie <mohammad.anies@nexsoft.co.id>
2022-09-18 21:52:14 +0700

    Challenge 02 Promise done. Commited by Muhammad Anies Wahdie.

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theater1 = await promiseTheaterIXX();
+    const theater2 = await promiseTheaterVGC();
+    const allTheater = [...theater1, ...theater2];
+
+    const arrEmosi = allTheater.filter((el) => el.hasil === emosi);
+    let jmlEmosi = `${emosi} : ${arrEmosi.length}`;
+
+    return jmlEmosi;
+  } catch(err){
+    console.log(err);
+  }
+
+}
 
 module.exports = {
   promiseOutput,
